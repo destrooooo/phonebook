6fbfdf728a8754c904eb33792bc32334c8c34f43
destrooooo <clementsilva@live.fr>
2025-04-16 16:54:29 +0200

    uibuild

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const cors = require("cors");
 
-app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 
 morgan.token("body", (req) => JSON.stringify(req.body));
