206d00aed6210603292f66932922687573e15694
destrooooo <clementsilva@live.fr>
2025-04-21 11:32:48 +0200

    uibuild

--- server/index.js
+++ server/index.js
@@ -12,6 +12,22 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.name, error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
 // Routes
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
@@ -30,28 +46,22 @@ app.get("/info", (req, res) => {
   });
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   Person.findById(req.params.id)
     .then((person) => {
       if (person) res.json(person);
       else res.status(404).end();
     })
-    .catch((error) => {
-      console.error(error);
-      res.status(400).send({ error: "malformatted id" });
-    });
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then(() => res.status(204).end())
-    .catch((error) => {
-      console.error(error);
-      res.status(400).send({ error: "malformatted id" });
-    });
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const { name, number } = req.body;
 
   if (!name || !number) {
@@ -67,10 +77,25 @@ app.post("/api/persons", (req, res) => {
 
     const newPerson = new Person({ name, number });
 
-    newPerson.save().then((savedPerson) => {
-      res.json(savedPerson);
-    });
+    newPerson
+      .save()
+      .then((savedPerson) => res.json(savedPerson))
+      .catch((error) => next(error));
   });
+});
+
+app.put("/api/persons/:id", (req, res, next) => {
+  const { number } = req.body;
+
+  Person.findByIdAndUpdate(
+    req.params.id,
+    { number },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 // Start server
@@ -78,3 +103,6 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
